5d96fb2ff6757cd1fae9ee6f0d7e29a3b6a9b994
galileu.kim <galileukim@gmail.com>
2021-01-12 11:20:49 -0500

    finalize generating campaign by party ex[enditure

--- source/modules/generate_party_spending.R
+++ source/modules/generate_party_spending.R
@@ -23,8 +23,31 @@ campaign <- input_filenames %>%
     map(fread)
 
 election <- fread(
-    here("data/raw/election_local.csv.gz")
-) 
+    here("data/raw/election_local.csv.gz"),
+    select = c("cod_ibge_6", "election_year", "party", "vote")
+)
+
+election_2020 <- fread(
+    here("data/raw/election_local_2020.csv"),
+    encoding = "Latin-1"
+) %>%
+    transmute(
+        cod_tse = SG_UE,
+        party = SG_PARTIDO,
+        vote = QT_VOTOS_NOMINAIS,
+        year = 2020
+    )
+
+identifiers <- fread(
+    here("data/raw/data_rosettastone.csv")
+) %>%
+    transmute(
+        cod_tse = codetse,
+        cod_ibge_6 = codeipea
+    ) %>%
+    mutate(
+        across(where(is.character), str_to_lower)
+    )
 
 # ---------------------------------------------------------------------------- #
 message("generate summary statistics")
@@ -35,11 +58,47 @@ campaign_party <- campaign %>%
 campaign_party <- campaign_party %>%
     filter(!is.na(cod_ibge_6))
 
+# generate party vote by m unicipality year
 vote_party <- election %>%
     rename(year = election_year) %>%
-    filter(year >= 2008 & position == "prefeito") %>%
+    filter(year >= 2008) %>%
     group_by(cod_ibge_6, party, year) %>%
     summarise(
         vote = sum(vote, na.rm = TRUE),
         .groups = "drop"
+    )
+
+vote_party <- vote_party %>%
+    bind_rows(
+        election_2020 %>% 
+            left_join(
+                identifiers,
+                by = "cod_tse"
+            ) %>%
+            group_by(
+                cod_ibge_6, party, year
+            ) %>%
+            summarise(
+                vote = sum(vote, na.rm = TRUE),
+                .groups = "drop"
+            )
+    ) %>%
+    arrange(
+        party, cod_ibge_6, year
+    )
+
+# join data
+campaign_party_vote <- campaign_party %>%
+    left_join(
+        vote_party,
+        by = c("cod_ibge_6", "party", "year")
+    )
+
+# ---------------------------------------------------------------------------- #
+message("writing out data")
+
+# write-out data
+campaign_party_vote %>%
+    fwrite(
+        here("data/clean/campaign_party_vote.csv")
     )
