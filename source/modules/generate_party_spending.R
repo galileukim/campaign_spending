ddf6e68bb2390c787aca50d806dcad4c930117f0
galileu.kim <galileukim@gmail.com>
2021-01-12 10:32:10 -0500

    add electoral count

--- source/modules/generate_party_spending.R
+++ source/modules/generate_party_spending.R
@@ -22,6 +22,24 @@ input_filenames <- list.files(
 campaign <- input_filenames %>%
     map(fread)
 
+election <- fread(
+    here("data/raw/election_local.csv.gz")
+) 
+
+# ---------------------------------------------------------------------------- #
+message("generate summary statistics")
 campaign_party <- campaign %>% 
     map_dfr(summarise_party_spending)
 
+# filter out na's (72 rows)
+campaign_party <- campaign_party %>%
+    filter(!is.na(cod_ibge_6))
+
+vote_party <- election %>%
+    rename(year = election_year) %>%
+    filter(year >= 2008 & position == "prefeito") %>%
+    group_by(cod_ibge_6, party, year) %>%
+    summarise(
+        vote = sum(vote, na.rm = TRUE),
+        .groups = "drop"
+    )
