68789d8f5b8d87a31c7e0dfe16305af7a59fbf0c
galileu.kim <galileukim@gmail.com>
2021-01-21 15:53:32 -0500

    finalize disaggregated visz

--- source/modules/visualize_campaign_spending.R
+++ source/modules/visualize_campaign_spending.R
@@ -30,81 +30,99 @@ campaign_party <- campaign_party %>%
 # ---------------------------------------------------------------------------- #
 message("generate visualizations")
 christian_parties <- c("prb", "psc")
-parties_to_plot <- c("prb", "psc", "pmdb", "dem", "psdb", "pt", "pp", "pr")
+disagg_parties <- c("prb", "psc", "pmdb", "dem", "psdb", "pt", "pp", "pr")
 
-# boxplots
-campaign_party_plot <-  campaign_party %>%
-    mutate(
-        party = if_else(party %in% parties_to_plot, party, "other") %>%
-            fct_relevel(
-                c(parties_to_plot, "other")
-            ),
-            year = as.factor(year)
-    )
-
-boxplot_value <- campaign_party_plot %>%
-    gg_boxplot(
-        x = party,
-        y = value_expense,
-        grouping = year,
-        lims = c(0, 8e4)
-    )
-
-boxplot_value_per_vote <- campaign_party_plot %>%
-    gg_boxplot(
-        year,
-        value_expense/vote,
-        grouping = party,
-        lims = c(0, 50)
-    )
-
-# point estimate: average
-campaign_party_plot_summary <- campaign_party_plot %>%
-    group_by(party, year) %>%
-    summarise(
-        value_expense_per_vote = sum(value_expense, na.rm = TRUE)/
-            sum(vote, na.rm = TRUE),
-        value_expense = mean(value_expense, na.rm = TRUE),
-        .groups = "drop"
-    ) 
-
-point_value <- campaign_party_plot_summary%>%
-    gg_point(
-        party,
-        value_expense,
-        grouping = year,
-        lims = c(0, 8e4)
-    ) +
-    coord_flip()
-
-point_value_per_vote <- campaign_party_plot_summary %>%
-    gg_point(
-        party,
-        value_expense_per_vote,
-        grouping = year,
-        lims = c(0, 20)
-    )
-
-plot <- list(
-    boxplot_value,
-    boxplot_value_per_vote,
-    point_value,
-    point_value_per_vote
+parties_to_plot <- list(
+    aggregated = christian_parties,
+    disaggregated = disagg_parties
 )
+plots_output <- list()
 
-filename <- sprintf(
-    here("figures/%s.pdf"),
-    c(
-        "boxplot_expense", 
-        "boxplot_expense_per_vote", 
-        "mean_expense", 
+for (parties in parties_to_plot) {
+    # generate tables for plotting
+    campaign_party_plot <- campaign_party %>%
+        mutate(
+            party = if_else(party %in% parties, party, "other") %>%
+                fct_relevel(
+                    c(parties, "other")
+                ),
+            year = as.factor(year)
+        )
+
+    campaign_party_plot_summary <- campaign_party_plot %>%
+        group_by(party, year) %>%
+        summarise(
+            value_expense_per_vote = sum(value_expense, na.rm = TRUE) /
+                sum(vote, na.rm = TRUE),
+            value_expense = mean(value_expense, na.rm = TRUE),
+            .groups = "drop"
+        )
+
+    # boxplots
+    boxplot_value <- campaign_party_plot %>%
+        gg_boxplot(
+            x = party,
+            y = value_expense,
+            grouping = year,
+            lims = c(0, 8e4)
+        )
+
+    boxplot_value_per_vote <- campaign_party_plot %>%
+        gg_boxplot(
+            party,
+            value_expense / vote,
+            grouping = year,
+            lims = c(0, 50)
+        )
+
+    # point estimate: average
+    point_value <- campaign_party_plot_summary %>%
+        gg_point(
+            party,
+            value_expense,
+            grouping = year,
+            lims = c(0, 8e4)
+        )
+
+    point_value_per_vote <- campaign_party_plot_summary %>%
+        gg_point(
+            party,
+            value_expense_per_vote,
+            grouping = year,
+            lims = c(0, 20)
+        )
+
+    repository <- if(length(parties) == 2) "aggregated" else "disaggregated"
+
+    plots_output[[repository]] <- list(
+        boxplot_value,
+        boxplot_value_per_vote,
+        point_value,
+        point_value_per_vote
+    )
+}
+
+plot_names <- c(
+        "boxplot_expense",
+        "boxplot_expense_per_vote",
+        "mean_expense",
         "mean_expense_per_vote"
     )
+
+filenames <- map(
+    list(
+        here("figures/%s.pdf"),
+        here("figures/disaggregated_%s.pdf")
+    ),
+    sprintf,
+    plot_names
 )
 
-pwalk(
-list(
-    plot = plot,
-    filename = filename
-),
-    ggsave)
+map2(
+    plots_output,
+    filenames,
+    ~ pwalk(
+        list(plot = .x, filename = .y),
+            ggsave
+        )
+    )
